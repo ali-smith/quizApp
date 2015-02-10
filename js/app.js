a03aacaea06dc4eb9b3a5653f4ca80afc8e10d80
Ali Smith <alipsmith@me.com>
2015-02-10 08:44:16 -0800

    new

--- js/app.js
+++ js/app.js
@@ -1,22 +1,51 @@
 $(document).ready(function(){
-
 	//welcome screen
-$(".startButton").on("click", function(){
-	$(".welcome").hide();
-	$(".quizTemplate").fadeIn(2500);
-	$(".fadeBackground").fadeIn();
-});
-
-	
-
-
-
-
-
-
-
-
+  $(".startButton").on("click", function(){
+	  $(".welcome").hide();
+	  $(".quizTemplate").fadeIn(2500);
+	  $(".fadeBackground").fadeIn();
+  });
+  //set current question
+  var currentQuestion = 1;
+  //html question
+  var addQuestion = function(){
+    $('.question').html(list[currentQuestion].question);
+  };
+  addQuestion();
+  //append answers
+   var addAnswers = function(){
+      for (i=0; i<=3; i++){
+  $('.answers').append('<i class="fa fa-circle-o"></i><div class="liDiv"><li>' + list[currentQuestion].answer[i] + '</li></div><br>'); 
+       }
+    };  
+  addAnswers();
+  //append right answer
+  // var addCorrectAnswer = function (){
+  //   $('.answers').append('<i class="fa fa-circle-o"></i><div class="liDiv"><li>' + list[currentQuestion].answer + '</li></div><br>');
+  // };
+  // addCorrectAnswer();
+  
+  //change question on click
+  $('.submit').on('click', function(){
+    // increase question by 1
+    var nextQuestion = currentQuestion++;
+    //change question text
+   addQuestion();
+    //remove previous answers
+    $('.answers').empty();
+    //add new, wrong answers
+    addAnswers();
+    //add new right answer
+  //   addCorrectAnswer();
+  });
 
 
 
 });//document ready
+
+
+
+
+
+
+
--- js/questions.js
+++ js/questions.js
@@ -1,71 +1,61 @@
 var Question1 = { 
 		questionNumber: 1,
 		question: '"Nebula" is the Latin word for:',
-		answer: 'cloud',
-        wrong: ['vague', 'dust','wormhole'],
+        answer: ['vague', 'cloud', 'dust','wormhole'],
         explain: '',
 };
 var Question2 = { 
 		questionNumber: 2,
-		question: 'What is a nebula?',
-		answer: 'a collection of dust and gas particles gathered from the universe',
-        wrong: ['smog spillover from L.A.', 'a group of prisms caused by galactic condensation',''],
-        explain: 'a ball of fire burning at varying temperatures',
+		question: 'What is a nebula?', 
+        answer: ['smog spillover from L.A.','a group of prisms caused by galactic condensation','a collection of dust and gas particles gathered from the universe','a ball of fire burning at varying temperatures'],
+        explain: '',
 };
 var Question3 = { 
 		questionNumber: 3,
 		question: 'Nebulae are formed by:',
-		answer: 'all of the above',
-        wrong: ['the gravitational collapse of gas', 'supernova explosions','the shedding of a planet’s outer layers'],
+        answer: ['the gravitational collapse of gas', 'supernova explosions','the shedding of a planet’s outer layers','all of the above'],
         explain: '',
 };
 var Question4 = { 
 		questionNumber: 4,
 		question: 'Some types of nebulae are: reflection, planetary, emission and:',
-		answer: 'absorption',
-        wrong: ['transcendent', 'imaginary','magnetic'],
+		answer: ['transcendent','absorption', 'imaginary','magnetic'],
         explain: '',
 };
 var Question5 = { 
 		questionNumber: 5,
 		question: 'What makes a reflection nebula glow?',
-		answer: 'the light from nearby stars',
-        wrong: ['hot embers floating around inside the cloud', 'a mysterious gooey substance','spontaneous electrical charge'],
+		answer: ['hot embers floating around inside the cloud', 'a mysterious gooey substance','spontaneous electrical charge','the light from nearby stars'],
         explain: '',
 };
 var Question6 = { 
 		questionNumber: 6,
 		question: 'A nebula is a breeding ground for:',
-		answer: 'young stars',
-        wrong: ['galaxies', 'aliens','black holes'],
+		answer: ['young stars','galaxies', 'aliens','black holes'],
         explain: '',
 };
 var Question7 = { 
 		questionNumber: 7,
 		question: 'What kind of nebula is home to a red giant?',
-		answer: 'planetary',
-        wrong: ['dark', 'supernova','beanstalk'],
+		answer: ['dark', 'planetary','supernova','beanstalk'],
         explain: '',
 };
 var Question8 = { 
 		questionNumber: 8,
 		question: 'Spectroscopy is the study of:',
-		answer: 'the spectrum through which a nebula\'s light passes',
-        wrong: ['gravity\'s impact on matter at varying densities', 'the specters that live inside emission nebulae','the inner world of dust specs found in a nebula'],
+		answer: ['gravity\'s impact on matter at varying densities', 'the spectrum through which a nebula\'s light passes','the specters that live inside emission nebulae','the inner world of dust specs found in a nebula'],
         explain: '',
 };
 var Question9 = { 
 		questionNumber: 9,
-		question: '',
-		answer: 'When a star forms in a nebula, it:',
-        wrong: ['gets its own reality show', 'grows exponentially as its gravitational pull engulfs the surrounding matter',' transforms the surrounding matter into orbiting planets'],
+		question: 'When a star forms in a nebula, it:',
+		answer: ['gets its own reality show', 'grows exponentially as its gravitational pull engulfs the surrounding matter', 'quickly pushes away the rest of the dust and gas with a powerful solar wind', 'transforms the surrounding matter into orbiting planets'],
         explain: '',
 };
 var Question10 = { 
 		questionNumber: 10,
 		question: 'The first astronomer to mention a group of "nebulous" stars in 150 A.D. was:',
-		answer: ' Ptolemy',
-        wrong: ['Galileo', 'Nostradamus','Spock'],
+		answer: ['Galileo', 'Ptolemy', 'Nostradamus','Spock'],
         explain: '',
 };	
 
