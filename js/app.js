96da4c93fcc8e81d1c155b00c762e886ff70e345
Ali Smith <alipsmith@me.com>
2015-02-12 11:27:52 -0800

    all

--- js/app.js
+++ js/app.js
@@ -24,10 +24,8 @@ $(document).ready(function(){
   $('i').on('click', function(){
   	if ($(this).hasClass('fa fa-circle-o')){
   		$(this).removeClass().addClass('fa fa-circle');
-  	} else {
-  		$(this).removeClass('fa fa-circle').addClass('fa fa-circle-o');
+  		$('i').not(this).removeClass().addClass('fa fa-circle-o');
   	}
-    	
   });	
 
   //change question on click
