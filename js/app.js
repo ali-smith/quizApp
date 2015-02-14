7243e20eedd65ca00bcfc4c989552683d4de101b
Ali Smith <alipsmith@me.com>
2015-02-14 08:44:09 -0800

    all

--- js/app.js
+++ js/app.js
@@ -2,19 +2,27 @@ $(document).ready(function(){
   
   var userAnswers = [];
   var nextQuestion;
+ 
+ 
+ // button to go directly to results page
+    $('.hide').on('click', function(){
+      $('.welcomeWrapper').hide();
+      $('.quizBackground').hide();
+      $('.quizResultsBackground').show();
+    });
 
 	//welcome screen
   $('.startButton').on('click', function(){
-	  $('.welcome').hide();
-	  $('.quizTemplate').fadeIn(2500);
-	  $('.fadeBackground, .quizBackground').fadeIn();
+	  $('.welcome').fadeOut(1500);
+	  $('.quizTemplate').fadeIn(2000);
+	  $('.fadeBackground, .quizBackground').fadeIn(2000);
   });
 
   //set current question
-  var currentQuestion = 1;
+  var currentQuestion = 0;
   //html question
   var addQuestion = function(){
-    $('.question').html(list[currentQuestion].question);
+    $('.question').html(list[currentQuestion].question).fadeIn(1200);
     $('.counterBar').html('<h1>countdown: ' + list[currentQuestion].questionNumber + '</h1>');  
   };
   addQuestion();
@@ -22,34 +30,30 @@ $(document).ready(function(){
   //append answers
   var addAnswers = function(){
       for (i=0; i<=3; i++){
-      $('.answersUL').append('<li class="answersLI"><i class="fa fa-circle-o"></i><span class="userSelection">' + list[currentQuestion].answers[i] + '</span></li><br>'); 
+      $('.answersUL').append('<li class="answersLI"><i class="fa fa-circle-o"></i><span class="userSelection">' + list[currentQuestion].answers[i] + '</span></li><br>').fadeIn(1200); 
        }
     };  
   addAnswers();
   
   //select answer
   	$('.answersUL').on('click', 'i', function(){
-  	if ($('i').hasClass('fa fa-circle-o')){
-  		$('i').removeClass().addClass('fa fa-circle');
-  		$('i').not(this).removeClass().addClass('fa fa-circle-o');
-  		}
+  	if ($('i:not(.fa-star, .fa-star-o, .fa-thumbs-down)').hasClass('fa fa-circle-o')){
+  		$('i:not(.fa-star, .fa-star-o, .fa-thumbs-down)').removeClass().addClass('fa fa-circle');
+  		$('i:not(.fa-star, .fa-star-o, .fa-thumbs-down)').not(this).removeClass().addClass('fa fa-circle-o');
+      }
   	});	
-  
+
   // find text in answer then push to userAnswers array
 	var pushUserSelection = function(){
 		var userAnswer = $('.answersUL > .answersLI > .fa-circle + .userSelection').text();
 		userAnswers.push(userAnswer);
 		console.log(userAnswers);
+    compareAnswers();
 	}
 
-  var quizOver = function(){
-   //if the quiz is over do this
-    if (userAnswers.length == 9){
-      $('.quizBackground').hide();
-      $('.quizResults').show();
-      
-      }
-  }      
+
+
+    
 
   //change question on click
   	$('.next').on('click', function(){
@@ -72,6 +76,32 @@ $(document).ready(function(){
       alert('must pick one!');
       }
   });
+
+  //if the quiz is over do this
+  var quizOver = function(){
+    if (userAnswers.length == 10){
+      $('.quizBackground').hide();
+      $('.quizResultsBackground').fadeIn(2500);
+      
+      }
+  }      
+ //compare userAnswers array with correct answers array
+  
+    //SOLUTION:
+    //http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript
+  //   var compareAnswers = function(){
+  //     for (var i = 0, i == 10, i++){
+  //     if (userAnswers[i] === list[currentQuestion].correctAnswer){
+  //       console.log('yup');
+  //       $('.accordionDiv').append('<article class="accordionArticle">
+  //   <h2 class="accordionHeader"><i class="fa fa-star"></i>' + list[currentQuestion].question + '</h2>
+  //   <p class="accordionAnswer">' + list[currentQuestion].explanation + '</p>
+  //   </article>');
+  //     }
+    
+  // };
+  
+  
 
 
 });//document ready
--- js/questions.js
+++ js/questions.js
@@ -2,71 +2,73 @@ var Question1 = {
 		questionNumber: 10,
 		question: '"Nebula" is the Latin word for:',
         answers: ['vague', 'cloud', 'dust','wormhole'],
-        correctAnswer: 'dust',
-        explain: '',
+        correctAnswer: 'cloud',
+        explanation: '',
 };
 var Question2 = { 
 		questionNumber: 9,
 		question: 'What is a nebula?', 
         answers: ['smog spillover from L.A.','a group of prisms caused by galactic condensation','a collection of dust and gas particles gathered from the universe','a ball of fire burning at varying temperatures'],
         correctAnswer: 'a collection of dust and gas particles gathered from the universe',
-        explain: '',
+        explanation: '',
 };
 var Question3 = { 
 		questionNumber: 8,
 		question: 'Nebulae are formed by:',
         answers: ['the gravitational collapse of gas', 'supernova explosions','the shedding of a planet’s outer layers','all of the above'],
         correctAnswer: 'the gravitational collapse of gas',
-        explain: '',
+        explanation: '',
 };
 var Question4 = { 
 		questionNumber: 7,
 		question: 'Some types of nebulae are: reflection, planetary, emission and:',
 		answers: ['transcendent','absorption', 'imaginary','magnetic'],
         correctAnswer: 'absorption',
-        explain: '',
+        explanation: '',
 };
 var Question5 = { 
 		questionNumber: 6,
 		question: 'What makes a reflection nebula glow?',
 		answers: ['hot embers floating around inside the cloud', 'a mysterious gooey substance','spontaneous electrical charge','the light from nearby stars'],
         correctAnswer: 'the light from nearby stars',
-        explain: '',
+        explanation: '',
 };
 var Question6 = { 
 		questionNumber: 5,
 		question: 'A nebula is a breeding ground for:',
 		answers: ['young stars','galaxies', 'aliens','black holes'],
         correctAnswer: 'young stars',
-        explain: '',
+        explanation: '',
 };
 var Question7 = { 
 		questionNumber: 4,
 		question: 'What kind of nebula is home to a red giant?',
 		answers: ['dark', 'planetary','supernova','beanstalk'],
         correctAnswer: 'planetary',
-        explain: '',
+        explanation: '',
 };
 var Question8 = { 
 		questionNumber: 3,
 		question: 'Spectroscopy describes:',
 		answers: ['gravity\'s impact on matter at varying densities', 'the spectrum through which a nebula\'s light passes','an uncomfortable procedure that specters over the age of 50 must endure','the inner world of dust specs found in a nebula'],
 		correctAnswer: 'the spectrum through which a nebula\'s light passes',
-        explain: '',
+        explanation: '',
 };
 var Question9 = { 
 		questionNumber: 2,
 		question: 'When a star forms in a nebula, it:',
 		answers: ['gets its own reality show', 'grows exponentially as its gravitational pull engulfs the surrounding matter', 'quickly pushes away the rest of the dust and gas with its solar wind', 'transforms the surrounding matter into orbiting planets'],
         correctAnswer: 'quickly pushes away the rest of the dust and gas with a powerful solar wind',
-        explain: '',
+        explanation: '',
 };
 var Question10 = { 
 		questionNumber: 1,
 		question: 'The first astronomer to mention a group of "nebulous" stars in 150 A.D. was:',
 		answers: ['Galileo', 'Ptolemy', 'Nostradamus','Spock'],
         correctAnswer: 'Ptolemy',
-        explain: '',
+        explanation: '',
 };	
 
-var list = [" ",Question1,Question2,Question3,Question4,Question5,Question6,Question7,Question8,Question9,Question10];
+var list = [Question1,Question2,Question3,Question4,Question5,Question6,Question7,Question8,Question9,Question10];
+
+// var correctAnswers = ['cloud', 'a collection of dust and gas particles gathered from the universe', 'the gravitational collapse of gas', 'absorption', 'the light from nearby stars', 'young stars', 'planetary', 'the spectrum through which a nebula\'s light passes', 'quickly pushes away the rest of the dust and gas with a powerful solar wind', 'Ptolemy']
